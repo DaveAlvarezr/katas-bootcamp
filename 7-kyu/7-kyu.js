8066f3402c53a3d80979adc86bd461e6c5a06290
Dave Alvarez <alvarezruizdavidJ@gmail.com>
2023-06-01 17:42:18 +0100

    add new katas ok

--- 7-kyu/7-kyu.js
+++ 7-kyu/7-kyu.js
@@ -20,6 +20,68 @@ function getAVG(arrayOfNumbers) {
   avg = sumNumbers / countNumbers;
   return avg;
 }
+
+function replaceVocalWithPosition(phrase) {
+  if (typeof phrase !== "string") {
+    return null;
+  }
+  const vocals = ["a", "e", "i", "o", "u"];
+
+  let phraseFormated = "";
+
+  for (let i = 0; i < phrase.length; i++) {
+    if (vocals.includes(phrase[i].toLowerCase())) {
+      phraseFormated += i + 1;
+    } else {
+      phraseFormated += phrase[i];
+    }
+  }
+
+  return phraseFormated;
+}
+
+function convertArrayStringsToArrayNumbers(arrayOfStrings) {
+  if (!Array.isArray(arrayOfStrings)) {
+    return [0];
+  }
+  const arrayOfNumbers = [];
+  for (const string of arrayOfStrings) {
+    let number = 0;
+    if (typeof string === "string") {
+      const numberOfString = parseInt(string);
+      if (!isNaN(numberOfString)) {
+        number = numberOfString;
+      }
+    }
+    arrayOfNumbers.push(number);
+  }
+  return arrayOfNumbers;
+}
+
+function removeValues(array1, array2) {
+  if (!Array.isArray(array1)) {
+    return [];
+  }
+  if (!Array.isArray(array2)) {
+    return array1;
+  }
+  const array = [];
+  for (const element of array1) {
+    if (!array2.includes(element)) {
+      array.push(element);
+    }
+  }
+  return array;
+}
+
+function buildArray(){
+  
+}
+
 module.exports = {
   getAVG,
+  replaceVocalWithPosition,
+  convertArrayStringsToArrayNumbers,
+  removeValues,
+  buildArray,
 };
